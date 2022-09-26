74d6ccce635b69ee7e26ebc32778c763d7d9cae2
Babli Kumari <skribabli@gmail.com>
2022-09-26 18:26:22 +0530

    ShowDigitalBusinessCard.java.

--- app/src/main/java/com/festivalbanner/digitalposterhub/Me/ShowDigitalBusinessCard.java
+++ app/src/main/java/com/festivalbanner/digitalposterhub/Me/ShowDigitalBusinessCard.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.festivalbanner.digitalposterhub.BuildConfig;
 import com.festivalbanner.digitalposterhub.R;
 
 public class ShowDigitalBusinessCard extends AppCompatActivity {
     TextView backPress;
     LinearLayout linearLayout2;
+    ImageView share, whatsapp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,8 @@ public class ShowDigitalBusinessCard extends AppCompatActivity {
     private void initMethod() {
         backPress = findViewById(R.id.backPress);
         linearLayout2 = findViewById(R.id.linearLayout2);
+        share = findViewById(R.id.share);
+        whatsapp = findViewById(R.id.whatsapp);
     }
 
     private void setAction() {
@@ -42,5 +47,50 @@ public class ShowDigitalBusinessCard extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareOnWhatsappMethod();
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareMethod();
+            }
+        });
+    }
+
+    private void shareOnWhatsappMethod() {
+        try {
+            //Share on Whatsapp Only
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Brand Make");
+            String shareMessage = "\nHello! Here's My Digital Business Card.\nYou can click on the link to open it.\n\n";
+            shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            shareIntent.setPackage("com.whatsapp");
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void shareMethod() {
+        try {
+            //Share
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Brand Make");
+            String shareMessage = "\nHello! Here's My Digital Business Card.\nYou can click on the link to open it.\n\n";
+            shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID + "\n";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
